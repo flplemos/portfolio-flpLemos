8670684592ebe1f95c7c85b22ea53a4ae82adea8
Felipe Lemos <139652554+flplemos@users.noreply.github.com>
2024-06-30 19:36:01 -0300

    responsiviade foi finalizada

--- script.js
+++ script.js
@@ -1,21 +1,56 @@
-let swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev"
-    },
-});
+const menuHamburguer = document.querySelector('.menu-hamburguer');
+const nav = document.querySelector('.navbar');
+const links = document.querySelectorAll('.navbar-links li a')
+
+let header = document.getElementById('header');
+let swiper = createSwiper(".mySwiper", "swiper-pagination", ".swiper-button-next", ".swiper-button-prev");
+let navbar = document.getElementsByClassName('test');
+let title = document.getElementById('titlenav');
+
+function createSwiper(container, pagination, nextButton, prevButton){
+    return new Swiper(container, {
+        slidesPerView: handleWidth(),
+        spaceBetween: 30,
+        pagination: {
+          el: pagination,
+          clickable: true,
+        },
+        navigation: {
+            nextEl: nextButton,
+            prevEl:  prevButton,
+        },
+    });
+}
 
 
-let header = document.getElementById('header')
-let navbar = document.getElementsByClassName('test')
-let title = document.getElementById('titlenav')
+function handleWidth(){
+    let getWidth = window.innerWidth || document.documentElement.clientWidth;
+    let slideShow = 3;
 
+    if (getWidth < 1001) {
+        slideShow = 2;
+    }
+
+    if (getWidth < 700) {
+        slideShow = 1;
+    }
+    return slideShow
+}
+
+menuHamburguer.addEventListener('click', () => {
+    nav.classList.toggle('active');
+})
+
+links.forEach(item => {
+    item.addEventListener('click', () => {
+        nav.classList.toggle('active');
+    })
+})
+
+window.addEventListener('resize', () => {
+    swiper.params.slidesPerView = handleWidth();
+    swiper.update();
+})
         
 window.addEventListener('scroll', () => {
     if (window.scrollY >= 50) {
